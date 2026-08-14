e1c9d70b432f9b5ad84fe4cef473f62a32182487
LuizHenrique406 <luiz.teixeira2803@gmail.com>
2026-08-14 09:58:37 -0300

    feitos

--- praticas/java/OlaMundo.java
+++ praticas/java/OlaMundo.java
@@ -1,8 +1,9 @@
-// public class OlaMundo {
-//     public static void main(String[] args) {
-//         System.out.print("Ola Mundo");
-//     }
-// }
-void main() {
-    IO.println("Ola Mundo Novo");
+public class OlaMundo {
+    public static void main(String[] args) {
+        System.out.print("Ola Mundo");
+    }
 }
+
+// void main() {
+//     System.out.println("Ola Mundo Novo");
+// }
--- praticas/java/lista01/CalMedia.java
+++ praticas/java/lista01/CalMedia.java
@@ -1,7 +1,17 @@
-void main() {
-    int num1 = Integer.parseInt(
-        IO.readln("Núemro 1: "));
-    int num2 = Integer.parseInt(IO.readln("Número 2: "));
-    int num3 = Integer.parseInt(IO.readln("Número 3: "));
-    System.out.print((num1 + num2 + num3) / 3);
+import java.util.Scanner;
+
+public class CalMedia {
+        public static void main(String[] args){
+            Scanner leitor = new Scanner(System.in);
+            System.out.print("Digite o valor de a: ");
+            int a = leitor.nextInt();
+            
+            System.out.print("Digite o valor de b: ");
+            int b = leitor.nextInt();
+            
+            System.out.print("Digite o valor de c: ");
+            int c = leitor.nextInt();
+            System.out.println("A média foi: " + ((a + b + c) / (3) ));
+            leitor.close();
+    }
 }
