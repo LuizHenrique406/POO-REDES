a6213d45374181d0d0a22bd56041ec8346815d1b
LuizHenrique406 <luiz.teixeira2803@gmail.com>
2026-08-14 10:22:03 -0300

    feito

---
+++ praticas/java/lista01/ParImpar.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ParImpar {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        System.out.print("Digite um valor: ");
+        int valor = leitor.nextInt();
+        if (valor % 2 == 0) {
+            System.out.println("Par");
+        }
+        else {
+            System.out.println("Ímpar");
+        }
+        leitor.close();
+    }
+}
