0d304ae9c8ecadacbf4e9dbeb4fcb26bba74755e
LuizHenrique406 <luiz.teixeira2803@gmail.com>
2026-08-14 10:12:40 -0300

    feito

---
+++ praticas/java/lista01/AreaCirculo.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class AreaCirculo{
+    public static void main(String[] args){
+        Scanner leitor = new Scanner(System.in);
+        System.out.print("Digite um valor: ");
+        int a = leitor.nextInt();
+        double pi = 3.14159;
+        System.out.println("A área do circulo foi: " + ((Math.pow(a,2)) * pi));
+        leitor.close();
+    }
+}
