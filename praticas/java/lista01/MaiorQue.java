93443d7c5e6c2656c7f935d5c4be90bb9d89c5c4
LuizHenrique406 <luiz.teixeira2803@gmail.com>
2026-08-14 10:50:31 -0300

    feito também

---
+++ praticas/java/lista01/MaiorQue.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class MaiorQue {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        System.out.print("Digite um valor: ");
+        int a = leitor.nextInt();
+
+        System.out.print("Digite um valor: ");
+        int b = leitor.nextInt();
+
+        System.out.print("Digite um valor: ");
+        int c = leitor.nextInt();
+
+        int maior = a;
+
+        if (b > maior) {
+            maior = b;
+        }
+        if (c > maior) {
+            maior = c;
+        }
+
+        System.out.print("O maior valor foi: " + maior);
+        leitor.close();
+    }
+}
