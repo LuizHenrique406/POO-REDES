1057f16a0b705d73d89d27ab3e3666c577a9b580
LuizHenrique406 <luiz.teixeira2803@gmail.com>
2026-09-09 22:24:26 -0300

    feitos

--- exercicios/java/listas/lista_beecrowd/dama.java
+++ exercicios/java/listas/lista_beecrowd/dama.java
@@ -1,19 +1,23 @@
 package listas.lista_beecrowd;
 import java.util.Scanner;
-
 public class dama {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int x1 = sc.nextInt();
-        int y1 = sc.nextInt();
-        int x2 = sc.nextInt();
-        int y2 = sc.nextInt();
-        if (x1 == y1 && x2 == y2) {
-            System.out.println(0);
-        } else if (x1 - x2 == y1 - y2) {
-            System.out.println(1);
-        } else {
-            System.out.println(2);
+        while (true) {
+            int x1 = sc.nextInt();
+            int y1 = sc.nextInt();
+            int x2 = sc.nextInt();
+            int y2 = sc.nextInt();
+            if (x1 == 0 && y1 == 0 && x2 == 0 && y2 == 0) {
+                break;
+            }
+            if (x1 == x2 && y1 == y2) {
+                System.out.println(0);
+            } else if (x1 == x2 || y1 == y2 || Math.abs(x1 - x2) == Math.abs(y1 - y2)) {
+                System.out.println(1);
+            } else {
+                System.out.println(2);
+            }
         }
         sc.close();
     }
--- exercicios/java/listas/lista_beecrowd/leds.java
+++ exercicios/java/listas/lista_beecrowd/leds.java
@@ -1,10 +1,27 @@
 package listas.lista_beecrowd;
-import java.util.Scanner;
+import java.util.Scanner;;
 
 public class leds {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int leds;
-                
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            String s = sc.next();
+            int leds = 0;
+            for (int j = 0; j < s.length(); j++) {
+                char letra = s.charAt(j);
+                switch (letra) {
+                    case '1' : leds += 2; break;
+                    case '2' : case '3': case '5': leds += 5; break;
+                    case '4' : leds += 4; break;
+                    case '0': case '6': case '9': leds += 6; break;
+                    case '7' : leds += 3; break;
+                    case '8' : leds += 7; break;
+                    default: leds += 0;
+                }
+            }
+            System.out.println(leds + " leds");
+        }
+        sc.close();
     }
 }
