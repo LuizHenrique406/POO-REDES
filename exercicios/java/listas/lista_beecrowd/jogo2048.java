283da2b0bd1069361e2901aa0c6a2c393f7c9f80
luiz.teixeira2803@gmail.com <luiz.teixeira2803@gmail.com>
2026-09-14 17:55:53 -0300

    Deus me ajude :(

---
+++ exercicios/java/listas/lista_beecrowd/jogo2048.java
@@ -0,0 +1,15 @@
+package listas.lista_beecrowd;
+import java.util.Scanner;
+
+public class jogo2048 {
+    public static void main(String[] args) {
+        int[] n = 
+        {0, 0, 0, 8,
+         0, 0, 2, 16,
+         0, 0, 2, 32,
+         2, 8, 16, 32
+        };
+        for (int i = 0; i < n.length; i++) {
+        }
+    }
+}
---
+++ exercicios/java/listas/lista_beecrowd/jogo_sapo.java
@@ -0,0 +1,10 @@
+package listas.lista_beecrowd;
+import java.util.Scanner;
+
+public class jogo_sapo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int pulo = sc.nextInt();
+        int
+    }
+}
