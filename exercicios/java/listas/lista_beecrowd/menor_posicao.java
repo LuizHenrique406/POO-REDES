f3e124098b53b9bb85be7525653c6b2f9451c566
LuizHenrique406 <luiz.teixeira2803@gmail.com>
2026-09-13 21:37:22 -0300

    feito

--- exercicios/java/listas/lista_beecrowd/menor_posicao.java
+++ exercicios/java/listas/lista_beecrowd/menor_posicao.java
@@ -6,16 +6,13 @@ public class menor_posicao {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] x = new int[n];
-        int menor = 0;
         int posicao = 0;
         for (int i = 0; i < n; i++) {
             x[i] = sc.nextInt();
         }
-        for (int j = 0; j < n - 1; j++) {
-            if (x[j] > x[j + 1]) {
-                menor = x[j + 1];
-                posicao = j;
-            } else {
+        int menor = x[0];
+        for (int j = 1; j < n; j++) {
+            if (x[j] <= menor) {
                 menor = x[j];
                 posicao = j;
             }
