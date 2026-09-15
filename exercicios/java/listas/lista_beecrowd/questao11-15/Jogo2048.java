7f750f145af86d5d2822f2969e5b9f39a573b260
LuizHenrique406 <luiz.teixeira2803@gmail.com>
2026-09-15 14:32:27 -0300

    .

--- exercicios/java/listas/lista_beecrowd/questao11-15/Jogo2048.java
+++ exercicios/java/listas/lista_beecrowd/questao11-15/Jogo2048.java
@@ -1,14 +1,24 @@
-import java.util.Scanner;
-
 public class Jogo2048 {
     public static void main(String[] args) {
+        int pos = 0;
         int[] n = 
         {0, 0, 0, 8,
          0, 0, 2, 16,
          0, 0, 2, 32,
          2, 8, 16, 32
         };
-        for (int i = 0; i < n.length; i++) {
+
+
+        
+        for (int j = 0; j < 4; j++) {
+            pos = j;
+            for (int i = 0; i < 4; i++) {
+                System.out.println(n[pos]);
+                pos += 4;
+            }
         }
+
+
+
     }
 }
--- exercicios/java/listas/lista_beecrowd/questao11-15/JogoSapo.java
+++ exercicios/java/listas/lista_beecrowd/questao11-15/JogoSapo.java
@@ -3,7 +3,16 @@ import java.util.Scanner;
 public class JogoSapo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int pulo = sc.nextInt();
-        int
+        int Pulo = sc.nextInt();
+        // int CanoQuant = sc.nextInt();
+        // int[] Canos = new int[CanoQuant];
+        int[] Canos = {1, 3, 6, 9, 7, 2, 4, 5, 8, 3};
+        for (int i = 0; i < Canos.length; i++) {
+            if ((Pulo - Canos[i]) <= Pulo) {
+                System.out.println("GAME OVER");
+                break;
+            }
+        }
+        System.out.println("YOU WIN");
     }
 }
