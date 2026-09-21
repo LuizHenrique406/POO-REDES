56a542eec315180e53ee5798c23bb3ed9fd3342c
luiz.teixeira2803@gmail.com <luiz.teixeira2803@gmail.com>
2026-09-21 18:03:07 -0300

    PAPO DE DOENTE, AMO ESSA VIDA DE DEV JAVA

--- exercicios/java/listas/lista_beecrowd/questao11-15/Jogo2048.java
+++ exercicios/java/listas/lista_beecrowd/questao11-15/Jogo2048.java
@@ -1,24 +1,37 @@
+import java.util.Scanner;
 public class Jogo2048 {
-    public static void main(String[] args) {
-        int pos = 0;
-        int[] n = 
-        {0, 0, 0, 8,
-         0, 0, 2, 16,
-         0, 0, 2, 32,
-         2, 8, 16, 32
-        };
-
-
-        
-        for (int j = 0; j < 4; j++) {
-            pos = j;
-            for (int i = 0; i < 4; i++) {
-                System.out.println(n[pos]);
-                pos += 4;
+    private static Scanner sc;
+    public static int[][] LerMatriz(int linhas, int colunas) {
+        int[][] jogo = new int[linhas][colunas];
+            for (int linha = 0; linha < 4; linha++) {
+                for (int coluna = 0; coluna < 4; coluna++) {
+                    jogo[linha][coluna] = sc.nextInt();
             }
         }
-
-
-
+        return LerMatriz(linhas, colunas);
+    }
+    public static boolean PodeMover(int a, int b) {
+        return a != 0 && (a == b || b == 0); 
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            int[][] jogo = LerMatriz(4,4);
+            boolean esquerda = false;
+            boolean direita = false;
+            boolean cima = false;
+            boolean baixo = false;
+            for (int j = 0; j < jogo.length; j++) {
+                for (int k = 0; k < jogo[j].length - 1; k++) {
+                    if (PodeMover(jogo[j][k], jogo[j][k + 1])) {direita = true;}
+                    if (PodeMover(jogo[j][k + 1], jogo[j][k])) {esquerda = true;}
+                    if (PodeMover(jogo[k][j], jogo[k + 1][j])) {baixo = true;}
+                    if (PodeMover(jogo[k + 1][j], jogo[k][j])) {cima = true;}
+                }
+            }
+            System.out.println((esquerda || direita || cima || baixo) ? "SIM" : "NAO");
+        }
+        sc.close();
     }
 }
